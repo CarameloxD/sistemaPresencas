999e980a4bfd0aab10cbfd5223e04b3291a97cc7
CarameloxD <joaopedromelo@live.com.pt>
2022-03-16 06:29:58 +0000

    Tentativas de Selects

--- controllers/home.go
+++ controllers/home.go
@@ -12,6 +12,9 @@ func GetStudentByNumber(c *gin.Context) {
 	services.OpenDatabase()
 	services.Db.Find(&user, c.Param("student_number"))
 
+	/*results := services.Db.Table("schedules, classes, subjects, classrooms, teachers, students, registrations").
+	Select("subjects.name, schedules.starting_time, schedules.ending_time").
+	Joins("where registrations.id_student = students.id and classes.id = registrations.id_class and subjects.id = schedules.id_subject and teachers.id = schedules.id_teacher and classrooms.id = schedules.id_classroom and classes.id = schedules.id_class")*/
 	if user.StudentNumber == 0 {
 		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "User not found!"})
 		return
--- model/Student.go
+++ model/Student.go
@@ -3,9 +3,10 @@ package model
 import "gorm.io/gorm"
 
 type Student struct {
-	gorm.Model `swaggerignore:"true"`
-	Id         int    `gorm:"primaryKey;autoIncrement:true" json:"id"`
-	Name       string `gorm:"size:255;not null" json:"name"`
-	Email      string `gorm:"size:255;not null" json:"email"`
-	Picture    string `json:"picture"`
+	gorm.Model    `swaggerignore:"true"`
+	Id            int    `gorm:"primaryKey;autoIncrement:true" json:"id"`
+	Name          string `gorm:"size:255;not null" json:"name"`
+	Email         string `gorm:"size:255;not null" json:"email"`
+	StudentNumber int    `json:"studentnumber"`
+	Picture       string `json:"picture"`
 }
